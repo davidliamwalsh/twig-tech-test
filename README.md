98a5c5c5efb2b2799163b1da66c73f77a47ce5d9
David Walsh <davidliamwalsh@yahoo.co.uk>
2019-10-22 15:25:21 +0100

    array chunk with jest (#1)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # twig-tech-test
-array chunking
+// Given an array of length >= 0, and a positive integer N, return the contents of the array divided into N equally sized arrays.
+
+// Where the size of the original array cannot be divided equally by N, the final part should have a length equal to the remainder.
+
+// groupArrayElements([​1​, ​2​, ​3​, ​4,​ ​5​], ​3​);
+// [ [ 1, 2 ], [ 3, 4 ], [ 5 ] ]
+
+// for result to go through array and split it into N size array
---
+++ src/script/array.js
@@ -0,0 +1,22 @@
+const groupArrayElements = (arr, len) => {
+  const chunkedArr = [];
+
+  // Loop through arr
+  arr.forEach(val => {
+    // Get last element
+    const last = chunkedArr[chunkedArr.length - 1];
+
+    // Check if last and if last length is equal to the chunk len
+    if (!last || last.length === len) {
+      chunkedArr.push([val]);
+    } else {
+      last.push(val);
+    }
+  });
+
+  return chunkedArr;
+}
+
+console.log(groupArrayElements([1,2,3,4,5], 2))
+
+module.exports = groupArrayElements
